18ba45b8ce8f1653f3adc7184faf4336e932fb8b
Danger <51889207+DangerDan88@users.noreply.github.com>
2019-09-26 09:07:52 -0600

    few changes new logic and using local storage

--- assets/giph.js
+++ assets/giph.js
@@ -2,12 +2,14 @@ $(document).ready(function () {
   /// array of initial buttons I want to create
   var buttons = ["Chicken and Waffles", "Pancakes", "Waffles", "Steak", "Oatmeal", "popeye's chicken sandwich",
     "turkey", "tortilla", "macaroni", "peanut butter", "bbq", "cinnamon roll", "bagel"];
-    // function to try and save local storage you need to JSON parse to turn data into an object
-    function localList(){
-      var localButton = JSON.parse(localStorage.getItem('button'));
-      
-      buttons = localButton;
-    }
+  // function to try and save local storage you need to JSON parse to turn data into an object
+  function localList() {
+    var localButton = JSON.parse(localStorage.getItem('button'));
+
+
+    buttons = localButton;
+  }
+
 
   // first you need to empty div so it does not repeat buttons then make function to dynamically make my buttons from values of my array
   $("#images").empty();
@@ -26,21 +28,22 @@ $(document).ready(function () {
     localStorage.setItem('button', JSON.stringify(buttons));
   }
   // calling functions
-  
-  makeButton();
   localList();
-  function searchGif(event){
+  makeButton();
+
+
+
+  // function to add new buttons that the user enters and you need to empty div again so no repeat buttons
+  $("#searchButton").on('click', function (event) {
     event.preventDefault();
     var value = $('#userInput').val().trim();
     $("#images").empty();
     buttons.push(value);
     makeButton();
 
-  }
-  // function to add new buttons that the user enters and you need to empty div again so no repeat buttons
-  $("#searchButton").on('click', searchGif); 
- 
+
+
 
   })
-
-
+  
+})
